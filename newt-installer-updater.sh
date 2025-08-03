0f828957cbecf057ff63d660d395d624959681cd
Nitin K <nitin.kadajiwale@outlook.com>
2025-08-03 18:43:11 +0530

    Update newt-installer-updater.sh

--- newt-installer-updater.sh
+++ newt-installer-updater.sh
@@ -79,7 +79,7 @@ if [[ -f "${SERVICE_FILE}" ]]; then
       read -p "Enable Newt Native Mode (y/N): " NEWT_NATIVE < /dev/tty
       read -p "Enable Docker Socket Access (y/N): " DOCKER_SOCKET < /dev/tty
       if [[ "${DOCKER_SOCKET}" =~ ^[Yy]$ ]]; then
-        read -p "Enter Docker Socket Path (ex. /var/run/docker.socket): " DOCKER_SOCKET_PATH < /dev/tty
+        read -p "Enter Docker Socket Path (ex. /var/run/docker.sock): " DOCKER_SOCKET_PATH < /dev/tty
       fi
       echo ""
     fi
